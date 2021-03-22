303d002c8d30a1fac4a66664214d232c45899ca0
Quentin EPRON <qepron@hotmail.fr>
2021-03-22 12:09:22 +0100

    V2

--- Main.java
+++ Main.java
@@ -1,15 +1,5 @@
-public class Main implements Runnable {
-
-    Window window = new Window(); 
-    public static void main(String[] args) {
-        new Thread(new Main()).start(); // Créer l'instance du jeu 
-    }
-
-    @Override
-    // Fonction update
-    public void run() {
-        while (true) {
-            window.repaint();
-        }
+class Main{
+    public static void main(String args[]){
+       Win window = new Win("2048");
     }
 }
